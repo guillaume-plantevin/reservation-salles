35f439bd8bceea86ead5f04e8836d2dafe52a584
guillaume-plantevin <guillaume.plantevin@laplateforme.io>
2021-01-04 19:35:24 +0100

    rajout page reservation-form.php

---
+++ reservation-form.php
@@ -0,0 +1,7 @@
+<?php
+    /*
+        Un formulaire de réservation de salle (reservation-form.php)
+        Ce formulaire contient les informations suivantes : 
+            titre, description, date de début, date de fin.
+    */
+?>
