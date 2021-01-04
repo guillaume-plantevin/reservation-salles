cc43867e375e17183cae1bb172f9a81214b64d4b
guillaume-plantevin <guillaume.plantevin@laplateforme.io>
2021-01-04 19:31:11 +0100

    rajout page profil.php

---
+++ profil.php
@@ -0,0 +1,7 @@
+<?php
+    /*
+        Une page permettant de modifier son profil (profil.php) :
+        Cette page possède un formulaire permettant 
+        à l’utilisateur de modifier son login et son mot de passe.
+    */
+?>
