600bc9af31901fb1c6c26636d605d14d938138a7
guillaume-plantevin <guillaume.plantevin@laplateforme.io>
2021-01-04 19:34:08 +0100

    rajout page reservation.php

---
+++ reservation.php
@@ -0,0 +1,11 @@
+<?
+    /*
+        Une page permettant de voir une réservation (reservation.php)
+        Cette page affiche le nom du créateur, 
+        le titre de l’événement, la description, l’heure de début et de fin. 
+        Pour savoir quel évènement afficher, 
+        vous devez récupérer l’id de l’événement en utilisant la méthode get. 
+        (ex : http://localhost/reservationsalles/evenement/?id=1​) 
+        Seuls les personnes connectées peuvent accéder aux événements.
+    */
+?>
