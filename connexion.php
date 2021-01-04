7e69a24ff6624a7533d45c1463a859e270b3d7c6
guillaume-plantevin <guillaume.plantevin@laplateforme.io>
2021-01-04 19:29:51 +0100

    rajout page connexion.php

---
+++ connexion.php
@@ -0,0 +1,11 @@
+<?php 
+    /*
+        Une page contenant un formulaire de connexion (connexion.php) :
+        Le formulaire doit avoir deux inputs : 
+        “login” et “password”. 
+        Lorsque le formulaire est validé, 
+        s’il existe un utilisateur en bdd correspondant à ces informations, 
+        alors l’utilisateur devient connecté 
+        et une (ou plusieurs) variables de session sont créées.
+    */
+?>
