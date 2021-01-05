12f67248e875594983df1027ed30afcacf194883
guillaume-plantevin <guillaume.plantevin@laplateforme.io>
2021-01-05 10:29:29 +0100

    affinage de la fonction randomRgb

--- reservation-form.php
+++ reservation-form.php
@@ -13,6 +13,25 @@
         <?php require_once('templates/header.php') ?>
         <main>
         <h1>Réservation de salle</h1>
+        <p>Le blahblah habituel...</p>
+        <!-- à envoyer en POST, car le descriptif peut-être long... -->
+        <form method="POST">
+            <label for="title">Titre:</label>
+            <input type="text" name="title" id="" /><br />
+
+            <label for="datetime">datetime</label>
+            <input type="datetime" name="datetime" id=""/><br />
+            
+            <label for=""></label>
+            <input type="datetime-local" name="" id=""/><br />
+            
+            <label for="description">Desciption:</label>
+            <textarea name="descrition" id="" cols="30" rows="10"></textarea>
+            
+
+            <input type="submit" value="Valider">
+            <input type="submit" value="Annuler">
+        </form>
         
         </main>
         <?php require_once('templates/footer.php') ?>
